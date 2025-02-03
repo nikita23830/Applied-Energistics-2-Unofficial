85301c7777360c13445cd180668fe713c5ff7a9e
Maya <10861407+serenibyss@users.noreply.github.com>
2025-02-03 15:06:40 -0600

    Fix NPE in stack size render (#664)

--- src/main/java/appeng/client/render/AppEngRenderItem.java
+++ src/main/java/appeng/client/render/AppEngRenderItem.java
@@ -102,7 +102,8 @@ public class AppEngRenderItem extends AERenderItem {
 
             // Display stack quantity
             final long amount = this.aeStack != null ? this.aeStack.getStackSize() : is.stackSize;
-            if (showStackSize && (amount > 0 || !this.aeStack.isCraftable())) {
+            final boolean isCraftable = this.aeStack != null && this.aeStack.isCraftable();
+            if (showStackSize && (amount > 0 || !isCraftable)) {
                 GL11.glDisable(GL11.GL_LIGHTING);
                 GL11.glPushMatrix();
                 this.drawStackSize(par4, par5, amount, fontRenderer, fontSize);
