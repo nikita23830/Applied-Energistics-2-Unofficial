nikita23830/Applied-Energistics-2-Unofficial

commit 73732a53020acad0960abb155fee361fbe9505e7

Refactor localization system: unify chat message and text locate with `Localization` interface and methods  (#791)

src/main/java/appeng/api/util/AEColor.java
@@ -16,14 +16,14 @@ package appeng.api.util;
 import java.util.Arrays;
 import java.util.List;
 
-import net.minecraft.util.StatCollector;
+import appeng.core.localization.Localization;
 
 /**
  * List of all colors supported by AE, their names, and various colors for display.
  * <p>
  * Should be the same order as Dyes, excluding Transparent.
  */
-public enum AEColor {
+public enum AEColor implements Localization {
 
     White("gui.appliedenergistics2.White", 0xBEBEBE, 0xDBDBDB, 0xFAFAFA),
 
@@ -111,8 +111,7 @@ public enum AEColor {
         return this == Transparent || color == Transparent || this == color;
     }
 
-    @Override
-    public String toString() {
-        return StatCollector.translateToLocal(this.unlocalizedName);
+    public String getUnlocalized() {
+        return unlocalizedName;
     }
 }

src/main/java/appeng/core/localization/PlayerMessages.java
@@ -10,10 +10,7 @@
 
 package appeng.core.localization;
 
-import net.minecraft.util.ChatComponentTranslation;
-import net.minecraft.util.IChatComponent;
-
-public enum PlayerMessages {
+public enum PlayerMessages implements Localization {
 
     ChestCannotReadStorageCell,
     InvalidMachine,
@@ -51,15 +48,7 @@ public enum PlayerMessages {
     MachineInOtherDim,
     MachineHighlighted;
 
-    public IChatComponent get() {
-        return new ChatComponentTranslation(this.getName());
-    }
-
-    public IChatComponent get(Object... args) {
-        return new ChatComponentTranslation(this.getName(), args);
-    }
-
-    public String getName() {
+    public String getUnlocalized() {
         return "chat.appliedenergistics2." + this.name();
     }
 }

src/main/java/appeng/api/config/PowerUnits.java
@@ -13,7 +13,9 @@
 
 package appeng.api.config;
 
-public enum PowerUnits {
+import appeng.core.localization.Localization;
+
+public enum PowerUnits implements Localization {
 
     AE("gui.appliedenergistics2.units.appliedenergstics"), // Native Units - AE Energy
     EU("gui.appliedenergistics2.units.ic2"), // IndustrialCraft 2 - Energy Units
@@ -47,5 +49,10 @@ public enum PowerUnits {
      */
     public double convertTo(final PowerUnits target, final double value) {
         return (value * this.conversionRatio) / target.conversionRatio;
+    }
+
+    @Override
+    public String getUnlocalized() {
+        return unlocalizedName;
     }
 }

src/main/java/appeng/block/AEBaseItemBlockChargeable.java
@@ -53,7 +53,7 @@ public class AEBaseItemBlockChargeable extends AEBaseItemBlock implements IAEIte
         toolTip.add(
                 GuiText.StoredEnergy.getLocal() + ": "
                         + NumberFormat.getNumberInstance(Locale.US).format(internalCurrentPower)
-                        + Platform.gui_localize(PowerUnits.AE.unlocalizedName)
+                        + PowerUnits.AE.getLocal()
                         + " - "
                         + MessageFormat.format(" {0,number,#.##%} ", percent));
     }

src/main/java/appeng/block/AEBaseTileBlock.java
@@ -73,6 +73,7 @@ public abstract class AEBaseTileBlock extends AEBaseBlock implements IAEFeature,
         super(mat, subName);
     }
 
+    /** setup all instance field before call this */
     @Override
     protected void setFeature(final EnumSet<AEFeature> f) {
         final AETileBlockFeatureHandler featureHandler = new AETileBlockFeatureHandler(f, this, this.featureSubName);

src/main/java/appeng/block/storage/BlockChest.java
@@ -62,7 +62,7 @@ public class BlockChest extends AEBaseTileBlock {
                 if (IntegrationRegistry.INSTANCE.isEnabled(IntegrationType.GT)
                         && GTUtility.isStackInList(p.getHeldItem(), GregTechAPI.sWireCutterList)) {
                     if (tg.toggleItemStorageCellLocking()) {
-                        p.addChatMessage(PlayerMessages.ChestLockingToggled.get());
+                        p.addChatMessage(PlayerMessages.ChestLockingToggled.toChat());
                     }
                     return true;
                 }
@@ -77,7 +77,7 @@ public class BlockChest extends AEBaseTileBlock {
 
                     tg.openGui(p, ch, cell, side);
                 } else {
-                    p.addChatMessage(PlayerMessages.ChestCannotReadStorageCell.get());
+                    p.addChatMessage(PlayerMessages.ChestCannotReadStorageCell.toChat());
                 }
             }
 

src/main/java/appeng/block/storage/BlockDrive.java
@@ -59,7 +59,7 @@ public class BlockDrive extends AEBaseTileBlock {
                 if (Loader.isModLoaded("dreamcraft") && IntegrationRegistry.INSTANCE.isEnabled(IntegrationType.GT)
                         && GTUtility.isStackInList(p.getHeldItem(), GregTechAPI.sWireCutterList)) {
                     if (tg.toggleItemStorageCellLocking()) {
-                        p.addChatMessage(PlayerMessages.DriveLockingToggled.get());
+                        p.addChatMessage(PlayerMessages.DriveLockingToggled.toChat());
                     }
                     return true;
                 }

src/main/java/appeng/client/gui/implementations/GuiCraftingCPU.java
@@ -22,7 +22,6 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.StatCollector;
 
 import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.lwjgl.opengl.GL11;
@@ -254,8 +253,8 @@ public class GuiCraftingCPU extends AEBaseGui implements ISortSource, IGuiToolti
             BlockPosHighlighter.highlightBlocks(
                     mc.thePlayer,
                     DimensionalCoord.readAsListFromNBT(data),
-                    PlayerMessages.InterfaceHighlighted.getName(),
-                    PlayerMessages.InterfaceInOtherDim.getName());
+                    PlayerMessages.InterfaceHighlighted.getUnlocalized(),
+                    PlayerMessages.InterfaceInOtherDim.getUnlocalized());
             mc.thePlayer.closeScreen();
         } else if (hoveredAEStack != null && btn == 2) {
             ((AEBaseContainer) inventorySlots).setTargetStack(hoveredAEStack);
@@ -638,11 +637,7 @@ public class GuiCraftingCPU extends AEBaseGui implements ISortSource, IGuiToolti
                 List<DimensionalCoord> blocks = DimensionalCoord.readAsListFromNBT(data);
 
                 ScheduledReason sr = ScheduledReason.values()[data.getInteger("ScheduledReason")];
-                if (sr != ScheduledReason.UNDEFINED) {
-                    lineList.add(
-                            StatCollector.translateToLocal(
-                                    "gui.tooltips.appliedenergistics2.scheduledreason." + sr.toString()));
-                }
+                if (sr != ScheduledReason.UNDEFINED) lineList.add(sr.getLocal());
 
                 if (blocks.isEmpty()) return;
                 for (DimensionalCoord blockPos : blocks) {

commit 1fb08f95ae2de6a59a51ec6a7fd9a7b7eabc4672

Fix crashes introduced by #791 (#796)

src/main/java/appeng/api/util/AEColor.java
@@ -16,6 +16,8 @@ package appeng.api.util;
 import java.util.Arrays;
 import java.util.List;
 
+import net.minecraft.util.StatCollector;
+
 import appeng.core.localization.Localization;
 
 /**
@@ -113,5 +115,10 @@ public enum AEColor implements Localization {
 
     public String getUnlocalized() {
         return unlocalizedName;
+    }
+
+    @Override
+    public String toString() {
+        return StatCollector.translateToLocal(this.unlocalizedName);
     }
 }

src/main/java/appeng/core/localization/PlayerMessages.java
@@ -10,6 +10,8 @@
 
 package appeng.core.localization;
 
+import net.minecraft.util.IChatComponent;
+
 public enum PlayerMessages implements Localization {
 
     ChestCannotReadStorageCell,
@@ -47,6 +49,21 @@ public enum PlayerMessages implements Localization {
     CraftingCantExtract,
     MachineInOtherDim,
     MachineHighlighted;
+
+    @Deprecated // kept for backward compat
+    public IChatComponent get() {
+        return toChat();
+    }
+
+    @Deprecated // kept for backward compat
+    public IChatComponent get(Object... args) {
+        return toChat(args);
+    }
+
+    @Deprecated // kept for backward compat
+    public String getName() {
+        return getUnlocalized();
+    }
 
     public String getUnlocalized() {
         return "chat.appliedenergistics2." + this.name();